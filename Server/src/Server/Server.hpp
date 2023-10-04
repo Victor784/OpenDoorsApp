f064347422b9b2c5cf7f5bc143da36724c4e65f2
Victor784 <victorpop839@yahoo.com>
2023-10-04 14:14:11 -0800

    KAN 3 : Server Api added

--- Server/src/Server/Server.hpp
+++ Server/src/Server/Server.hpp
@@ -4,6 +4,23 @@
 #include "../Connection/IConnection.hpp"
 #include "../Connection/WSConnection.hpp"
 
+
+enum class EntranceType
+{
+    Window,
+    RabatableWindow,
+    Door,
+    RabatableDoor
+};
+
+enum class Position
+{
+    East,
+    West,
+    South,
+    North
+};
+
 class Server{
     private:
         IConnection& connection;
@@ -14,5 +31,20 @@ class Server{
         void run();
         void process (std::string message);
         std::string trim(std::string message);
+        void createDataBase();
+        void executeCommand(std::string message);
+
+        //DB elem control
+        void switchStateForEntrance(int id);
+        void addEntrance(int roomId, EntranceType type, Position position);
+        void deleteEntrance(int entranceId);
+        void changeEntrance(int entranceId, EntranceType newType, Position newPosition);
+        void addRoom(std::string name, int level);
+        void changeRoom(int roomId, std::string newName, int newLevel);
+        void deleteRoom(int roomId);
+        void changeAddressCountry(std::string newCountryName);
+        void changeAddressCity(std::string newCityName);
+        void changeAddressStreet(std::string newStreetName);
+        void changeAddressNr(int newNr);
         ~Server() = default;
 };
